0a88adcf622b99f98a5c5036083c847cd4ee362d
siddhant <ssiddhant3030@gmail.com>
2022-11-18 10:40:37 +0530

    few things added for stir

---
+++ stir/models/example/delhi_india.sql
@@ -0,0 +1,14 @@
+{{ config(
+  materialized='table',
+   indexes=[
+      {'columns': ['_airbyte_ab_id'], 'type': 'hash'}
+    ]
+
+) }}
+
+select
+{{ dbt_utils.star(from= source('survey-cto', 'delhi'), except=['diet_delhi', 's1']) }},
+'India' AS country, 'delhi' AS region, diet_delhi as sub_region, COALESCE(s1, ad1) as s1
+from {{ source('survey-cto', 'delhi') }} 
+
+-- select * from {{ source('survey-cto', 'delhi') }}
