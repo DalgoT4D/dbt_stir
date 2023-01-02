47d57b44f125602239d5648b7966d1175c15df13
vinod-rajasekaran <23266611+vinod-rajasekaran@users.noreply.github.com>
2023-01-02 22:23:45 +0530

    Fix typo in subindicator name

--- models/production/classroom_surveys/derived/classroom_surveys_normalized.sql
+++ models/production/classroom_surveys/derived/classroom_surveys_normalized.sql
@@ -15,6 +15,6 @@ CASE
 WHEN subindicator IN ('s1','s2','s3','s4') THEN 'Safety' 
 WHEN subindicator IN ('c1','c2') THEN 'Curiosity & Critical Thinking'
 WHEN subindicator IN ('e1','e2') THEN 'Engagement'
-WHEN subindicator IN ('se1','se2', 'se3', 'se4', 'se5') THEN 'Engagement'
+WHEN subindicator IN ('se1','se2', 'se3', 'se4', 'se5') THEN 'Self Esteem'
 ELSE 'Other' END AS behavior from merged_normalized
 ORDER BY "submissiondate","KEY"
