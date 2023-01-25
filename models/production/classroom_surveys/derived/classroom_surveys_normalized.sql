64fd366325ff16f299803cb1dfe82d370be5c68a
vinod-rajasekaran <flans-boules0n@icloud.com>
2023-01-25 12:09:06 +0530

    Added c3 indicator

--- models/production/classroom_surveys/derived/classroom_surveys_normalized.sql
+++ models/production/classroom_surveys/derived/classroom_surveys_normalized.sql
@@ -5,7 +5,7 @@
 
 with merged_normalized AS (SELECT "KEY","submissiondate","country","region","sub_region","forms","observation_term","meeting",
        unnest(array['s1','s2','s3','s4',
-                    'c1','c2',
+                    'c1','c2','c3',
                     'e1','e2',
                     'se1','se2','se3','se4','se5',
                     'cc1','cc2', 'cc3', 'cc4', 'cc5',
@@ -48,7 +48,7 @@ FROM {{ref('classroom_surveys_merged')}}
 select *, 
 CASE 
 WHEN subindicator IN ('s1','s2','s3','s4') THEN 'Safety' 
-WHEN subindicator IN ('c1','c2') THEN 'Curiosity & Critical Thinking'
+WHEN subindicator IN ('c1','c2','c3') THEN 'Curiosity & Critical Thinking'
 WHEN subindicator IN ('e1','e2') THEN 'Engagement'
 WHEN subindicator IN ('se1','se2', 'se3', 'se4', 'se5') THEN 'Self Esteem'
 WHEN subindicator IN ('cc1','cc2', 'cc3', 'cc4', 'cc5') THEN 'Intentional Teaching'
