90e7d63ea8a302865a252622010fbc57f04c9cae
siddhant <ssiddhant3030@gmail.com>
2023-03-10 09:07:50 +0530

    fixed cro for forms

--- models/intermediate/classroom_surveys/int_classroom_surveys_delhi.sql
+++ models/intermediate/classroom_surveys/int_classroom_surveys_delhi.sql
@@ -8,8 +8,21 @@
 ) }}
 
 select
-{{ dbt_utils.star(from= source('source_classroom_surveys', 'delhi'), except=['diet_delhi', 's1', 'date', 'date_coaching','starttime','endtime','submissiondate','completiondate']) }},
-'India' AS country, 'delhi' AS region, diet_delhi as sub_region, COALESCE(s1, ad1) as s1, to_date(coalesce(date,date_coaching), 'Mon, DD YYYY') as observation_date,
+{{ dbt_utils.star(from= source('source_classroom_surveys', 'delhi'), except=['diet_delhi', 's1', 's2', 's3', 'e1', 'e2','c1', 'c1a', 'c2', 'c2a', 'c3', 'se1', 'se2', 'se3', 'date', 'date_coaching','starttime','endtime','submissiondate','completiondate']) }},
+'India' AS country, 'delhi' AS region, diet_delhi as sub_region, to_date(coalesce(date,date_coaching), 'Mon, DD YYYY') as observation_date,
+COALESCE(cro1, s1) as s1,
+COALESCE(cro2, s2) as s2,  
+COALESCE(cro3, s3) as s3, 
+COALESCE(cro4, e1) as e1, 
+COALESCE(cro5, e2) as e2, 
+COALESCE(cro7, c1) as c1,
+COALESCE(cro7a, c1a) as c1a, 
+COALESCE(cro8, c2) as c2, 
+COALESCE(cro8a, c2a) as c2a, 
+COALESCE(cro9, c3) as c3, 
+COALESCE(cro10, se1) as se1,
+COALESCE(cro11, se2) as se2, 
+COALESCE(cro12, se3) as se3, 
 to_timestamp(starttime,'Mon, DD YYYY HH:MI:SS AM') AS starttime,
 to_timestamp(endtime,'Mon, DD YYYY HH:MI:SS AM') AS endtime,
 to_timestamp(completiondate,'Mon, DD YYYY HH:MI:SS AM') AS completiondate,
