677db93e784632b430eb42b84cabcdc7508cd415
siddhant <ssiddhant3030@gmail.com>
2023-03-16 02:52:42 +0530

    uganda fixes

--- models/intermediate/classroom_surveys/int_classroom_surveys_uganda.sql
+++ models/intermediate/classroom_surveys/int_classroom_surveys_uganda.sql
@@ -8,10 +8,21 @@
 ) }}
 
 select
+CASE 
+  When forms_uganda = 'cc_ug' then 'coaching_calls_uganda'
+  When forms_uganda = 'del_ins' then 'district_education_leader_institute'
+  When forms_uganda = 'cct_ins' then 'center_coordinating_tutors_institute'
+  When forms_uganda = 'sel_ins' then 'school_education_leader_institute'
+  When forms_uganda = 'asshu_nb' then 'association_of_secondary_school_headteachers_of_uganda_national_bootcamp'
+  When forms_uganda = 'dam_ug' then 'district_alignment_meeting_uganda'
+  When forms_uganda = 'nm_ug' then 'network_meeting_uganda'
+  When forms_uganda = 'cro_ug' then 'classroom_observation_uganda'
+  When forms_uganda = 'mid_term_ug' then 'mid_term_meetups_Uganda'
+END As forms,
 {{ dbt_utils.star(from= source('source_classroom_surveys', 'uganda'), except=[district_bunyoro,district_kigezi,
     district_masaka, district_rwenzori, district_central, 's1', 's2', 's3', 'e1', 'e2','c1', 'c1a', 'c2', 'c2a', 'c3', 'se1', 'se2', 'se3', 'date', 'date_coaching','starttime','endtime','submissiondate','completiondate']) }},
 'Uganda' AS country, location_uganda AS region, coalesce (district_bunyoro,district_kigezi,district_masaka, district_rwenzori, district_central) as sub_region,
-to_date(coalesce(date,date_coaching), 'Mon, DD YYYY') as observation_date, forms_uganda as forms,
+to_date(coalesce(date,date_coaching), 'Mon, DD YYYY') as observation_date,
 COALESCE(cro1, s1) as s1,
 COALESCE(cro2, s2) as s2,  
 COALESCE(cro3, s3) as s3, 
