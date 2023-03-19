359e170fe9afa113861d7ea78ad6600d3d065f90
siddhant <ssiddhant3030@gmail.com>
2023-03-19 23:08:17 +0530

    location uganda and indonesia removed

--- models/production/classroom_surveys/derived/classroom_surveys_normalized.sql
+++ models/production/classroom_surveys/derived/classroom_surveys_normalized.sql
@@ -3,7 +3,34 @@
 ) }}
 
 
-with merged_normalized AS (SELECT "KEY","submissiondate","country","region","sub_region","forms","observation_term","meeting",
+with merged_normalized AS
+       (SELECT "KEY","submissiondate","country","region","sub_region","forms","observation_term","meeting",
+       CASE 
+        When forms = 'nm_indo' then 'network_meeting_indonesia'
+        When forms = 'cc_indo' then 'coaching_calls_indonesia'
+        When forms = 'cro_indo' then 'classroom_observation_indonesia'
+        When forms = 'dcm_indo' then 'district_coordination_meeting_indonesia'
+        When forms = 'cc' then 'coaching_calls'
+        When forms = 'cro' then 'classroom_observations'    
+        When forms = 'nm' then 'network_meeting'
+        When forms = 'nm_coart' then 'network_meeting_co-academic'
+        When forms = 'dmpc' then 'district_monthly_progress_checks'
+        When forms = 'nm' then 'network_meeting'
+        When forms = 'nm_art' then 'network_meeting_academic'
+        When forms = 'el_ins' then 'education_leaders_institute'
+        When forms = 'elm_ins' then 'education_leader_manager_institute'
+        When forms = 'dam' then 'district_alignment_meeting'
+        When forms = 'dc_ins' then 'district_champion_institute'
+        When forms = 'cc_ug' then 'coaching_calls_uganda'
+        When forms = 'del_ins' then 'district_education_leader_institute'
+        When forms = 'cct_ins' then 'center_coordinating_tutors_institute'
+        When forms = 'sel_ins' then 'school_education_leader_institute'
+        When forms = 'asshu_nb' then 'association_of_secondary_school_headteachers_of_uganda_national_bootcamp'
+        When forms = 'dam_ug' then 'district_alignment_meeting_uganda'
+        When forms = 'nm_ug' then 'network_meeting_uganda'
+        When forms = 'cro_ug' then 'classroom_observation_uganda'
+        When forms = 'mid_term_ug' then 'mid_term_meetups_Uganda'
+       END As forms_verbose,
        unnest(array['s1','s2','s3','s4',
                     'c1','c2','c3',
                     'e1','e2',
@@ -67,3 +94,5 @@ WHEN subindicator IN ('cro13ai_behavior_engagement','cro13ai_behavior_safety','c
                     'cro13av_growth_mindset','cro13av_normalising_error','cro13b','cro13c') THEN 'Additional CRO Indicators'
 ELSE 'Other' END AS behavior from merged_normalized
 ORDER BY "submissiondate","KEY"
+
+
