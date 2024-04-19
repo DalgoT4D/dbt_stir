caaef83396d7995f07bc7100a0828744363827fe
Abhishek-N <abhisheknshetty97@gmail.com>
2024-04-19 22:46:02 +0530

    update the models for comparing

---
+++ models/dev_intermediate/classroom_surveys/dev_classroom_surveys_karnataka.sql
@@ -0,0 +1,13 @@
+{{ config(
+  materialized='table',
+) }}
+
+select
+forms,
+{{ dbt_utils.star(from= ref('dev_staging_karnataka'), except=['forms', 'district_kt', 'date', 'date_coaching','starttime','endtime','submissiondate','"CompletionDate"', '_airbyte_karnataka_stir_bm_2022_hashid']) }},
+'India' AS country, 'karnataka' AS region, district_kt as sub_region, to_date(coalesce(date,date_coaching), 'Mon, DD YYYY') as observation_date,
+to_timestamp(starttime,'Mon, DD YYYY HH:MI:SS AM') AS starttime,
+to_timestamp(endtime,'Mon, DD YYYY HH:MI:SS AM') AS endtime,
+to_timestamp("CompletionDate",'Mon, DD YYYY HH:MI:SS AM') AS completiondate,
+to_timestamp("SubmissionDate",'Mon, DD YYYY HH:MI:SS AM') AS submissiondate
+from {{ ref("dev_staging_karnataka") }}
