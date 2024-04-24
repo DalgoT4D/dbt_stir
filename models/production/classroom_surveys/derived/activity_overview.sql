22bb5e4b0d3d03c8b6487a311e4260c38260c684
Abhishek-N <abhisheknshetty97@gmail.com>
2024-04-24 18:43:27 +0530

    distinct key with the filters

--- models/production/classroom_surveys/derived/activity_overview.sql
+++ models/production/classroom_surveys/derived/activity_overview.sql
@@ -2,33 +2,27 @@
   materialized='table'
 ) }}
 
-WITH filtered_merged AS (
-    SELECT 
-        *,
-        CASE 
-            WHEN forms IN (
-                'asshu_nb','asshu_ins','cct_ins','sel_ins','del_ins','dpo_nb',
-                'dam_ug','mid_term_ug','el_ins','elm_ins','dam','dmpc',
-                'dcm_indo','ss_indo','sp_indo','dcac_indo'
-            ) THEN 'Institutes'
-            WHEN forms IN ('nm','nm_art','nm_coart','nm_ug','nm_indo') THEN 'Network Meetings'
-            WHEN forms IN ('cro','cro_ug','cro_indo') THEN 'Classroom Observations'
-            WHEN forms IN ('cc','cc_ug','cc_indo') THEN 'Coaching Calls'
-            ELSE 'Other'
-        END AS activity_type
-    FROM 
-        {{ ref('classroom_surveys_normalized') }}
-)
-
 SELECT 
     coalesce(region, 'Unknown') as region,
-    SubmissionDate,
+    submissiondate,
     "KEY",
-    COUNT(*) FILTER (WHERE activity_type = 'Institutes') AS institutes_count,
-    COUNT(*) FILTER (WHERE activity_type = 'Network Meetings') AS network_meetings_count,
-    COUNT(*) FILTER (WHERE activity_type = 'Classroom Observations') AS classroom_observations_count,
-    COUNT(*) FILTER (WHERE activity_type = 'Coaching Calls') AS coaching_calls_count
+    forms,
+    sub_region,
+    COUNT(DISTINCT "KEY") FILTER (WHERE forms IN (
+        'asshu_nb','asshu_ins','cct_ins','sel_ins','del_ins','dpo_nb',
+        'dam_ug','mid_term_ug','el_ins','elm_ins','dam','dmpc',
+        'dcm_indo','ss_indo','sp_indo','dcac_indo'
+    )) AS institutes_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE forms IN (
+        'nm', 'nm_art', 'nm_coart', 'nm_ug', 'nm_indo'
+    )) AS network_meetings_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE forms IN (
+        'cro', 'cro_ug', 'cro_indo'
+    )) AS classroom_observations_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE forms IN (
+        'cc', 'cc_ug', 'cc_indo'
+    )) AS coaching_calls_count
 FROM 
-    filtered_merged
+    {{ ref('classroom_surveys_normalized') }}
 GROUP BY 
-    region, SubmissionDate, "KEY"
+    region, submissiondate, "KEY", forms, sub_region
