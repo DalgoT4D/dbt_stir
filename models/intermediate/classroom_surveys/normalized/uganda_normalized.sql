689d12a1b46a0c6d3ac2d3b10b0a0ad8543d2ea6
Abhishek-N <abhisheknshetty97@gmail.com>
2024-04-24 09:28:51 +0530

    add deduplicates

---
+++ models/intermediate/classroom_surveys/normalized/uganda_normalized.sql
@@ -0,0 +1,18 @@
+{{ config(
+  materialized='table'
+) }}
+
+with my_cte as ({{
+  flatten_json(
+    model_name = source('source_classroom_surveys_dev', 'uganda'),
+    json_column = 'data'
+)
+}})
+
+
+{{ dbt_utils.deduplicate(
+    relation='my_cte',
+    partition_by='"KEY"',
+    order_by='"KEY" desc',
+   )
+}}
