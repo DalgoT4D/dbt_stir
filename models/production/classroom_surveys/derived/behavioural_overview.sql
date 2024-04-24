e6160e84eaaf65498ae5d2bfa189ddaa261ad225
Abhishek-N <abhisheknshetty97@gmail.com>
2024-04-24 18:43:27 +0530

    distinct key with the filters

--- models/production/classroom_surveys/derived/behavioural_overview.sql
+++ models/production/classroom_surveys/derived/behavioural_overview.sql
@@ -2,31 +2,26 @@
   materialized='table'
 ) }}
 
-WITH filtered_data AS (
-    SELECT 
-        *,
-        CASE 
-            WHEN behavior IN ('Curiosity & Critical Thinking', 'Engagement', 'Safety', 'Self Esteem') AND score IN (1, 2, 3) THEN 'Behavioral Overview'
-            WHEN behavior = 'Curiosity & Critical Thinking' AND score IN (1, 2, 3) THEN 'Curiosity & Critical Thinking'
-            WHEN behavior = 'Engagement' THEN 'Engagement'
-            WHEN behavior = 'Safety' THEN 'Safety'
-            WHEN behavior = 'Self Esteem' THEN 'Self Esteem'
-            ELSE 'Other'
-        END AS behavior_type
-    FROM 
-        {{ ref('classroom_surveys_normalized') }}
-)
-
 SELECT 
     coalesce(region, 'Unknown') as region,
-    SubmissionDate,
+    submissiondate,
+    score,
+    behavior,
     "KEY",
-    COUNT(*) FILTER (WHERE behavior_type = 'Behavioral Overview') AS behavioral_overview_count,
-    COUNT(*) FILTER (WHERE behavior_type = 'Curiosity & Critical Thinking') AS curiosity_critical_thinking_count,
-    COUNT(*) FILTER (WHERE behavior_type = 'Engagement') AS engagement_count,
-    COUNT(*) FILTER (WHERE behavior_type = 'Safety') AS safety_count,
-    COUNT(*) FILTER (WHERE behavior_type = 'Self Esteem') AS self_esteem_count
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior = 'Curiosity & Critical Thinking' AND score IN (1, 2, 3)) AS curiosity_critical_thinking_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior = 'Engagement' AND score IN (1, 2, 3)) AS engagement_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior = 'Safety' AND score IN (1, 2, 3)) AS safety_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior = 'Self Esteem' AND score IN (1, 2, 3)) AS self_esteem_count,
+    COUNT(DISTINCT "KEY") FILTER (
+        WHERE (
+            (behavior = 'Curiosity & Critical Thinking' AND score IN (1, 2, 3)) OR
+            (behavior = 'Engagement' AND score IN (1, 2, 3)) OR
+            (behavior = 'Safety' AND score IN (1, 2, 3)) OR
+            (behavior = 'Self Esteem' AND score IN (1, 2, 3))
+        )
+    ) AS behavioral_overview_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior = 'Intentional Teaching' AND score >= 0) AS intentional_teaching_count
 FROM 
-    filtered_data
+    {{ ref('classroom_surveys_normalized') }}
 GROUP BY 
-    region, SubmissionDate, "KEY"
+    region, submissiondate, "KEY", behavior, score
