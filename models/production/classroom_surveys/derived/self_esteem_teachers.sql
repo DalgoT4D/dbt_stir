41d3c8bf85f9fbb34b8eec974feac2b8927518a7
Abhishek-N <abhisheknshetty97@gmail.com>
2024-05-03 13:17:44 +0530

    self esteem teacher model

---
+++ models/production/classroom_surveys/derived/self_esteem_teachers.sql
@@ -0,0 +1,34 @@
+{{ config(
+  materialized='table',
+  schema=generate_schema_name('prod', this)
+) }}
+
+SELECT 
+    coalesce(region, 'Unknown') as region,
+    submissiondate,
+    score,
+    behavior,
+    "KEY",
+    COUNT("KEY") as count_keys,
+    CASE
+        WHEN (score IN (1)) THEN 'Self Esteem - A Few'
+        WHEN (score IN (2)) THEN 'Self Esteem - About Half'
+        WHEN (score IN (3)) THEN 'Self Esteem - Most'
+        ELSE 'Other'
+    END AS score_category,
+    CASE 
+        WHEN subindicator IN ('se1') THEN 'Peer collaboration'
+        WHEN subindicator IN ('se2') THEN 'Sought facilitator support'
+        WHEN subindicator IN ('se3') THEN 'Recognition & celebration'
+        WHEN subindicator IN ('se4') THEN 'Sought peer support'
+        WHEN subindicator IN ('se5') THEN 'Shared developmental feedback'
+        ELSE 'Other' 
+    END AS subindicator_category
+FROM 
+    {{ ref('classroom_surveys_normalized') }}
+WHERE 
+    score in (1, 2, 3)
+    AND behavior in ('Self Esteem')
+    AND forms in ('nm_indo','nm_art','nm','nm_ug','nm_coart')
+GROUP BY 
+    region, submissiondate, "KEY", behavior, score, subindicator
