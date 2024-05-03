7233bac0707588cfa2c60cf9b1635eba47ba21bf
Abhishek-N <abhisheknshetty97@gmail.com>
2024-05-03 17:45:03 +0530

    trendline models

--- models/production/classroom_surveys/derived/activity_overview.sql
+++ models/production/classroom_surveys/derived/activity_overview.sql
@@ -23,7 +23,16 @@ SELECT
     )) AS classroom_observations_count,
     COUNT(DISTINCT "KEY") FILTER (WHERE forms IN (
         'cc', 'cc_ug', 'cc_indo'
-    )) AS coaching_calls_count
+    )) AS coaching_calls_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior IN (
+        'Curiosity & Critical Thinking'
+    ) AND score in (1, 2, 3)) AS c_and_ct_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior IN (
+        'Engagement')) AS engagement_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior IN (
+        'Safety')) AS safety_count,
+    COUNT(DISTINCT "KEY") FILTER (WHERE behavior IN (
+        'Self Esteem')) as self_esteem_count
 FROM 
     {{ ref('classroom_surveys_normalized') }}
 GROUP BY 
---
+++ models/production/classroom_surveys/derived/behavioral_trendline.sql
@@ -0,0 +1,17 @@
+{{ config(
+  materialized='table',
+  schema=generate_schema_name('prod', this)
+) }}
+
+SELECT  region,
+        DATE_TRUNC('month', submissiondate) AS submissiondate,
+        "KEY",
+        score,
+        forms,
+        sub_region,
+        sum(CAST(case
+                    when score=3 then 1
+                    else 0
+                end AS FLOAT)) / CAST(count(1) AS FLOAT) AS score_most
+FROM {{ ref('classroom_surveys_normalized') }}
+GROUP BY DATE_TRUNC('month', submissiondate), "KEY", score, forms, region, sub_region
